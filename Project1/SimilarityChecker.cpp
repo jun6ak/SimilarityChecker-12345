240057e385c6655c5903edfe78d9b98c0956b5af
junyoung <jun6ak@gmail.com>
2024-07-03 16:59:38 +0900

    [feature] get score length 60

---
+++ Project1/SimilarityChecker.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+using namespace std;
+
+class SimilarityChecker
+{
+public:
+    int getScoreOfLength(const string& str1, const string& str2)
+    {
+        return 60;
+    }
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include "../Project1/SimilarityChecker.cpp"
 
-TEST(SimilarityTest, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(SimilarityTest, GetScoreOfLength) {
+    string str1 = "ASD";
+    string str2 = "DSA";
+
+    SimilarityChecker checker;
+    EXPECT_EQ(60, checker.getScoreOfLength(str1, str2));
 }
